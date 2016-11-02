e895f4d4b4fb8242f4acd88eb4b8f454a471e515
Mugi Rachmat <infomugi@gmail.com>
2016-11-02 18:23:02 +0700

    Remove
    Hapus file sampah :dancers:

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // Yii::setPathOfAlias('bootstrap', dirname(__FILE__).'/../extensions/bootstrap');
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'KUPAKUPI',
+	'name'=>'KUPA KUPI STORE',
 	'theme' => 'store',
 	'language'=>'id',
 	'timeZone'=>'Asia/Jakarta',	
@@ -27,7 +27,7 @@ return array(
 		
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>false,
+			'password'=>'admin',
 		// 	// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 			'generatorPaths' => array('bootstrap.gii'),
